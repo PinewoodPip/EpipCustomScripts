4a5517c9c7f7e0616c10fde2d4be88924156b177
Kamil <lekamilthemage@gmail.com>
2025-06-24 19:59:00 +0200

    Add PartyPooper script

---
+++ PartyPooper.lua
@@ -0,0 +1,11 @@
+---------------------------------------------
+-- Disables April Fools and birthday shenanigans.
+---------------------------------------------
+
+Epip.IsPipBirthday = function ()
+    return false
+end
+
+Epip.IsAprilFools = function ()
+    return false
+end
